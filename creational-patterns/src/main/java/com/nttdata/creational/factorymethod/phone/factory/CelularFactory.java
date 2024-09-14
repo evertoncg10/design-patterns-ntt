f144ca2d7eb3b91811e0fae235d7b534ea17eb7c
evertoncg10 <evertoncg10@outlook.com>
2024-09-14 14:56:56 -0300

    Implementação design pattern Factory Method(Tradicional) em Java. Retirada de modificador desnecessário.

--- creational-patterns/src/main/java/com/nttdata/creational/factorymethod/phone/factory/CelularFactory.java
+++ creational-patterns/src/main/java/com/nttdata/creational/factorymethod/phone/factory/CelularFactory.java
@@ -3,7 +3,7 @@ package com.nttdata.creational.factorymethod.phone.factory;
 import com.nttdata.creational.factorymethod.phone.factory.model.Celular;
 
 public interface CelularFactory {
-    public Celular criarCelular();
+    Celular criarCelular();
 
 }
 
